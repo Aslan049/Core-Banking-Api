bfcf3431586857a79f435f6a597415157860021f
Aslan Korkmaz <aslankorkmaz049@gmail.com>
2025-12-22 18:27:30 +0300

    test(customer-service): add and improve unit tests

--- src/test/java/com/aslankorkmaz/Core/Banking/API/service/CustomerServiceImpTest.java
+++ src/test/java/com/aslankorkmaz/Core/Banking/API/service/CustomerServiceImpTest.java
@@ -1,6 +1,7 @@
 package com.aslankorkmaz.Core.Banking.API.service;
 
 import com.aslankorkmaz.Core.Banking.API.dto.customer.request.CustomerCreateRequest;
+import com.aslankorkmaz.Core.Banking.API.dto.customer.request.CustomerUpdateRequest;
 import com.aslankorkmaz.Core.Banking.API.dto.customer.response.CustomerResponse;
 import com.aslankorkmaz.Core.Banking.API.entity.customer.Customer;
 import com.aslankorkmaz.Core.Banking.API.exception.CustomerAlreadyExists;
@@ -11,14 +12,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -31,106 +31,190 @@ public class CustomerServiceImpTest {
     @Mock
     private ICustomerRepository customerRepository;
 
+    @Captor
+    private ArgumentCaptor<Customer> customerArgumentCaptor;
+
 
     @Test
     void should_get_customer_by_id() {
+        // given
         Long id = 1L;
         Customer customer = new Customer();
         customer.setId(id);
-        Mockito.when(customerRepository.findById(id)).thenReturn(Optional.of(customer));
-        customerServiceImp.getCustomerById(id);
-        verify(customerRepository).findById(id);
+        customer.setFirstName("FirstName");
+        customer.setLastName("LastName");
+        customer.setEmail("Email");
+        when(customerRepository.findById(id))
+                .thenReturn(Optional.of(customer));
 
+        // when
+        CustomerResponse result = customerServiceImp.getCustomerById(id);
+
+        // then
+        assertEquals(id, result.getId());
+        assertEquals("FirstName", result.getFirstName());
+        assertEquals("LastName", result.getLastName());
+        assertEquals("Email", result.getEmail());
+
+        verify(customerRepository, times(1)).findById(id);
     }
 
     @Test
     void should_throw_exception_when_customer_id_not_found() {
+        // given
         Long id = 1L;
-        Customer customer = new Customer();
-        customer.setId(id);
-        Mockito.when(customerRepository.findById(id)).thenReturn(Optional.empty());
-        CustomerNotFoundException exception = assertThrows(CustomerNotFoundException.class, () -> customerServiceImp.getCustomerById(id));
-        Assertions.assertThat(exception.getMessage()).isEqualTo("Customer with id " + id + " not found");
+        when(customerRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        // when
+        CustomerNotFoundException exception =
+                assertThrows(CustomerNotFoundException.class,
+                        () -> customerServiceImp.getCustomerById(id));
+
+        // then
+        Assertions.assertThat(exception.getMessage())
+                .isEqualTo("Customer with id " + id + " not found");
+
     }
 
-    @DisplayName("Should create customer and save correct fields")
     @Test
+    @DisplayName("Should create customer and save correct fields")
     void should_create_customer() {
+        // given
         CustomerCreateRequest request = new CustomerCreateRequest();
         request.setFirstName("FirstName");
         request.setLastName("LastName");
         request.setEmail("Email");
         request.setIdentityNumber("IdentityNumber");
-        Mockito.when(customerRepository.save(Mockito.any(Customer.class))).thenReturn(new Customer());
+        when(customerRepository.save(any(Customer.class)))
+                .thenReturn(new Customer());
 
-        ArgumentCaptor<Customer> captor = ArgumentCaptor.forClass(Customer.class);
-
+        // when
         customerServiceImp.createCustomer(request);
 
-        verify(customerRepository).save(captor.capture());
+        // then
+        verify(customerRepository, times(1))
+                .save(customerArgumentCaptor.capture());
 
-        Customer savedCustomer = captor.getValue();
-        Assertions.assertThat(savedCustomer.getFirstName()).isEqualTo("FirstName");
-        Assertions.assertThat(savedCustomer.getLastName()).isEqualTo("LastName");
-        Assertions.assertThat(savedCustomer.getEmail()).isEqualTo("Email");
+        Customer savedCustomer = customerArgumentCaptor.getValue();
+        assertThat(savedCustomer.getFirstName()).isEqualTo("FirstName");
+        assertThat(savedCustomer.getLastName()).isEqualTo("LastName");
+        assertThat(savedCustomer.getEmail()).isEqualTo("Email");
+        assertThat(savedCustomer.getIdentityNumber()).isEqualTo("IdentityNumber");
+
     }
 
-    @DisplayName("Should throw exception if customer already exists by identity number")
     @Test
-    void should_throw_exception_when_customer_already_exists() {
+    @DisplayName("Should throw exception if customer already exists by identity number")
+    void should_throw_exception_when_customer_already_exists_by_identity_number() {
+        // given
         String identityNumber = "IdentityNumber";
         CustomerCreateRequest request = new CustomerCreateRequest();
         request.setIdentityNumber(identityNumber);
-        Mockito.when(customerRepository.existsByIdentityNumber(identityNumber)).thenReturn(true);
-        CustomerAlreadyExists exception = assertThrows(CustomerAlreadyExists.class, () -> customerServiceImp.createCustomer(request));
-        Assertions.assertThat(exception.getMessage()).isEqualTo("Customer Already Exists with Identity Number: "+identityNumber);
-        verify(customerRepository,never()).save(Mockito.any(Customer.class));
+        when(customerRepository.existsByIdentityNumber(identityNumber))
+                .thenReturn(true);
+
+        // when
+        CustomerAlreadyExists exception = assertThrows(CustomerAlreadyExists.class,
+                () -> customerServiceImp.createCustomer(request));
+
+        // then
+       assertThat(exception.getMessage()).
+                isEqualTo("Customer Already Exists with Identity Number: " + identityNumber);
+       verify(customerRepository, times(1)).existsByIdentityNumber(identityNumber);
+       verify(customerRepository,never()).save(any(Customer.class));
     }
 
     @Test
-    void Should_return_customers_list() {
+    void should_return_customers_list() {
+        // given
         Customer customer = new Customer();
         customer.setId(1L);
         customer.setFirstName("FirstName");
         customer.setLastName("LastName");
         customer.setEmail("Email");
         customer.setIdentityNumber("IdentityNumber");
+        when(customerRepository.findAll()).thenReturn(List.of(customer));
 
-
-        Mockito.when(customerRepository.findAll()).thenReturn(List.of(customer));
-
+        // when
         List<CustomerResponse> result = customerServiceImp.getAllCustomers();
 
+        // then
         CustomerResponse customerResponse = result.get(0);
 
-        Assertions.assertThat(customerResponse.getId()).isEqualTo(1L);
-        Assertions.assertThat(customerResponse.getFirstName()).isEqualTo("FirstName");
-        Assertions.assertThat(customerResponse.getLastName()).isEqualTo("LastName");
-        Assertions.assertThat(customerResponse.getEmail()).isEqualTo("Email");
-        Assertions.assertThat(customerResponse.getIdentityNumber()).isEqualTo("IdentityNumber");
+        assertThat(customerResponse.getId()).isEqualTo(1L);
+        assertThat(customerResponse.getFirstName()).isEqualTo("FirstName");
+        assertThat(customerResponse.getLastName()).isEqualTo("LastName");
+        assertThat(customerResponse.getEmail()).isEqualTo("Email");
+        assertThat(customerResponse.getIdentityNumber()).isEqualTo("IdentityNumber");
 
         verify(customerRepository,times(1)).findAll();
     }
 
     @Test
     void should_delete_customer_by_id() {
+        // given
         Long id = 1L;
         Customer customer = new Customer();
         customer.setId(id);
         Mockito.when(customerRepository.findById(id)).thenReturn(Optional.of(customer));
+
+        // when
         customerServiceImp.deleteCustomer(id);
-        verify(customerRepository).delete(customer);
+
+        // then
+        verify(customerRepository, times(1)).findById(id);
+        verify(customerRepository, times(1)).delete(customer);
     }
 
     @Test
     void should_throw_exception_if_customer_id_not_found_for_delete() {
+        // given
+        Long id = 1L;
+        Mockito.when(customerRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        // when
+        CustomerNotFoundException exception = assertThrows(CustomerNotFoundException.class,
+                () -> customerServiceImp.deleteCustomer(id));
+
+        // then
+        assertThat(exception.getMessage())
+                .isEqualTo("Customer with id " + id + " not found");
+        verify(customerRepository, never()).delete(any());
+    }
+
+    @Test
+    void should_update_customer_by_id() {
+        // given
         Long id = 1L;
         Customer customer = new Customer();
         customer.setId(id);
-        Mockito.when(customerRepository.findById(id)).thenReturn(Optional.empty());
-        CustomerNotFoundException exception = assertThrows(CustomerNotFoundException.class, () -> customerServiceImp.deleteCustomer(id));
-        Assertions.assertThat(exception.getMessage()).isEqualTo("Customer with id " + id + " not found");
-        verify(customerRepository,never()).delete(customer);
+        when(customerRepository.findById(id)).thenReturn(Optional.of(customer));
+
+        CustomerUpdateRequest request = new CustomerUpdateRequest();
+        request.setFirstName("FirstName");
+        request.setLastName("LastName");
+        request.setEmail("Email");
+        when(customerRepository.save(any(Customer.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        // when
+        CustomerResponse customerResponse = customerServiceImp.updateCustomer(request,id);
+
+        // then
+        verify(customerRepository).findById(id);
+        verify(customerRepository, times(1))
+                .save(customerArgumentCaptor.capture());
+
+        Customer updatedCustomer = customerArgumentCaptor.getValue();
+        assertThat(updatedCustomer.getFirstName()).isEqualTo("FirstName");
+        assertThat(updatedCustomer.getLastName()).isEqualTo("LastName");
+        assertThat(updatedCustomer.getEmail()).isEqualTo("Email");
+
+        assertThat(customerResponse.getFirstName()).isEqualTo("FirstName");
+        assertThat(customerResponse.getLastName()).isEqualTo("LastName");
+        assertThat(customerResponse.getEmail()).isEqualTo("Email");
     }
 
 }
